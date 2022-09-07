32aac887cb036b41c23de7cb04d0e934e31d8953
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-07 16:39:08 -0700

    more sql queries

--- intro.js
+++ intro.js
@@ -5,14 +5,22 @@ const db = require('./models')
 const userCRUD = async () => {
     try {
         // CREATE 
-        const newUser = await db.user.create({
-            firstName: 'Loebita',
-            lastName: 'Fawk',
-            age: 42,
-            email: 'ddaw@b.com'
-        })
-        console.log(newUser)
+        // const newUser = await db.user.create({
+        //     firstName: 'Loebita',
+        //     lastName: 'Fawk',
+        //     age: 42,
+        //     email: 'ddaw@b.com'
+        // })
+        // console.log(newUser)
         // READ
+        // const allUsers = await db.user.findAll()
+        // only some users.
+        // const someUsers = await db.user.findAll({
+        //     where: {
+        //         firstName: 'Sebastian'
+        //     }
+        // })
+        console.log(someUsers)
         // UPDATE
         // DESTROY
     } catch(err) {
