d02f6a779dd5db85eb69208e4f149b0d0b376957
Manion-sebastian <Sjmanion123@gmail.com>
2022-09-07 16:30:02 -0700

    setting up functionality

---
+++ intro.js
@@ -0,0 +1,14 @@
+const db = require('./models')
+
+// db.user.sequelizeMethod()
+
+const userCRUD = async () => {
+    try {
+        // CREATE 
+        // READ
+        // UPDATE
+        // DESTROY
+    } catch(err) {
+        console.warn(err)
+    }
+}
